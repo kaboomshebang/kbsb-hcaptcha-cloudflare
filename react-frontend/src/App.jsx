bd6c4cc2331fcad7ddc2a5c9cb601975ea4132df
Fred Snyder <safe.sand1626@fastmail.com>
2022-11-14 17:04:23 +0000

    Move text from homepage to nav

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -19,31 +19,11 @@ function App() {
 					<header>
 						<h1>kbsb-hCaptcha-Cloudflare</h1>
 						<h2>Verify a hCaptcha token with a Cloudflare Worker.</h2>
-						<hr />
-						<p>
-							Uses the{' '}
-							<a href="https://github.com/hCaptcha/react-hcaptcha">react-hcaptcha</a>{' '}
-							package. And a standard <code>FormData()</code> form. The form data is
-							send to a Cloudflare worker.
-						</p>
-						<p>
-							You can find the source code in the{' '}
-							<a href="http://www.github.com/kaboomshebang/kbsb-hcaptcha-cloudflare">
-								Github repo
-							</a>
-							. The Cloudflare code is located in the <code>cloudflare-worker</code>{' '}
-							directory. The React code in the <code>react-frontend</code> directory.
-						</p>
-						<p>
-							A succesful verification results in a{' '}
-							<code>hCaptcha token accepted</code> server response.
-						</p>
 					</header>
 				</article>
 
-				<article>
-					<Form onResponse={setResponse} header="Contact Form" />
-				</article>
+				<Form onResponse={setResponse} header="Contact Form" />
+
 				<Console response={response} />
 			</main>
 			<footer>Copyright Kaboom Shebang</footer>
--- react-frontend/src/components/UI/Nav.jsx
+++ react-frontend/src/components/UI/Nav.jsx
@@ -6,5 +6,23 @@ export const Nav = () => {
 			<h1>Kaboom Shebang</h1>
 			<button>Menu</button>
 		</nav>
+
+				<p>
+					Uses the <a href="https://github.com/hCaptcha/react-hcaptcha">react-hcaptcha</a>{' '}
+					package. And a standard <code>FormData()</code> form. The form data is send to a
+					Cloudflare worker.
+				</p>
+				<p>
+					You can find the source code in the{' '}
+					<a href="http://www.github.com/kaboomshebang/kbsb-hcaptcha-cloudflare">
+						Github repo
+					</a>
+					. The Cloudflare code is located in the <code>cloudflare-worker</code>{' '}
+					directory. The React code in the <code>react-frontend</code> directory.
+				</p>
+				<p>
+					A succesful verification results in a <code>hCaptcha token accepted</code>{' '}
+					server response.
+				</p>
 	);
 };
