617c3debd801cafe98a3ffd321847ebd156ce0f9
Fred Snyder <safe.sand1626@fastmail.com>
2022-11-14 15:07:25 +0000

    Add Nav component

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import { Nav } from './components/UI/Nav';
 import { Form } from './components/Form/Form';
 import { Console } from './components/UI/Console';
 
@@ -9,38 +10,44 @@ function App() {
 	const [response, setResponse] = useState('');
 
 	return (
-		<main className="App">
-			<article>
-				<header>
-					<h1>kbsb-hCaptcha-Cloudflare</h1>
-					<h2>Verify a hCaptcha token with a Cloudflare Worker.</h2>
-					<hr />
-					<p>
-						Uses the{' '}
-						<a href="https://github.com/hCaptcha/react-hcaptcha">react-hcaptcha</a>{' '}
-						package. And a standard <code>FormData()</code> form. The form data is send
-						to a Cloudflare worker.
-					</p>
-					<p>
-						You can find the source code in the{' '}
-						<a href="http://www.github.com/kaboomshebang/kbsb-hcaptcha-cloudflare">
-							Github repo
-						</a>
-						. The Cloudflare code is located in the <code>cloudflare-worker</code>{' '}
-						directory. The React code in the <code>react-frontend</code> directory.
-					</p>
-					<p>
-						A succesful verification results in a <code>hCaptcha token accepted</code>{' '}
-						server response.
-					</p>
-				</header>
-			</article>
+		<>
+			<header>
+				<Nav />
+			</header>
+			<main className="App">
+				<article>
+					<header>
+						<h1>kbsb-hCaptcha-Cloudflare</h1>
+						<h2>Verify a hCaptcha token with a Cloudflare Worker.</h2>
+						<hr />
+						<p>
+							Uses the{' '}
+							<a href="https://github.com/hCaptcha/react-hcaptcha">react-hcaptcha</a>{' '}
+							package. And a standard <code>FormData()</code> form. The form data is
+							send to a Cloudflare worker.
+						</p>
+						<p>
+							You can find the source code in the{' '}
+							<a href="http://www.github.com/kaboomshebang/kbsb-hcaptcha-cloudflare">
+								Github repo
+							</a>
+							. The Cloudflare code is located in the <code>cloudflare-worker</code>{' '}
+							directory. The React code in the <code>react-frontend</code> directory.
+						</p>
+						<p>
+							A succesful verification results in a{' '}
+							<code>hCaptcha token accepted</code> server response.
+						</p>
+					</header>
+				</article>
 
-			<article>
-				<Form onResponse={setResponse} header="Contact Form" />
-			</article>
-			<Console response={response} />
-		</main>
+				<article>
+					<Form onResponse={setResponse} header="Contact Form" />
+				</article>
+				<Console response={response} />
+			</main>
+			<footer>Copyright Kaboom Shebang</footer>
+		</>
 	);
 }
 
---
+++ react-frontend/src/components/UI/Nav.jsx
@@ -0,0 +1,10 @@
+import * as css from './Nav.module.css';
+
+export const Nav = () => {
+	return (
+		<nav className={css.nav}>
+			<h1>Kaboom Shebang</h1>
+			<button>Menu</button>
+		</nav>
+	);
+};
