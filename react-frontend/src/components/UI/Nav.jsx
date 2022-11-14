53659e1995750188474c7f5e5f2cb448363f556b
Fred Snyder <safe.sand1626@fastmail.com>
2022-11-14 17:05:05 +0000

    Finish nav component template and styles

--- react-frontend/src/components/UI/Nav.jsx
+++ react-frontend/src/components/UI/Nav.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
+
 import * as css from './Nav.module.css';
 
 export const Nav = () => {
+	const [showMenu, setShowMenu] = useState(false);
+
+	const baseUrl = 'https://assets.kbsb.app/svg/';
+	const logo = 'kaboom_shebang_logo.svg';
+	const burger = 'icon_hamburger.svg';
+
+	const clickHandler = () => {
+		setShowMenu((prevState) => !prevState);
+	};
+
 	return (
-		<nav className={css.nav}>
-			<h1>Kaboom Shebang</h1>
-			<button>Menu</button>
-		</nav>
+		<div className={css.navWrapper}>
+			<nav className={css.nav}>
+				<img src={baseUrl + logo} alt="Kaboom Shebang" />
+				<button className={css.button} onClick={clickHandler}>
+					<img src={baseUrl + burger} alt="Burger" />
+				</button>
+			</nav>
+			<div className={css.menu} data-show-menu={showMenu}>
+				<header>
+					<h3>Information</h3>
+					<button onClick={clickHandler}>Close</button>
+				</header>
 
 				<p>
 					Uses the <a href="https://github.com/hCaptcha/react-hcaptcha">react-hcaptcha</a>{' '}
@@ -24,5 +44,7 @@ export const Nav = () => {
 					A succesful verification results in a <code>hCaptcha token accepted</code>{' '}
 					server response.
 				</p>
+			</div>
+		</div>
 	);
 };
